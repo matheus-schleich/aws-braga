e43ee85d5d97caa231244392e1667252ef938339
Matheus <math.schleich@outlook.com>
2025-10-28 11:41:07 +0000

    Add script to print server credentials from arguments

---
+++ base_de_dados.py
@@ -0,0 +1,10 @@
+# Deve receber 3 valores passados por argumento e imprimir o servidor, usuário e senha
+import sys
+
+server = sys.argv[1]
+username = sys.argv[2]
+password = sys.argv[3]
+
+print("Servidor:", server)
+print("Usuário:", username)
+print("Senha:", password)
