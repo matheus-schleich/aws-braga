65f861ea8b809eb2e922a2511e1768798b316451
Matheus <math.schleich@outlook.com>
2025-10-28 10:00:16 +0000

    Add script to print command line arguments

---
+++ exemplo.py
@@ -0,0 +1,12 @@
+import sys
+ 
+print("Nome do script: ", sys.argv[0])
+print("Parametros: ", sys.argv[1:])
+ 
+v1 = sys.argv[1]
+v2 = sys.argv[2]
+v3 = sys.argv[3]
+
+print(v1)
+print(v2)
+print(v3)
